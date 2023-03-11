16037c42c75ada90bf0a8159595f7ec45d60494b
KorirMoze <korirkipsmoze@gmail.com>
2023-03-11 13:32:29 +0300

    registerred my models

--- csvreader/admin.py
+++ csvreader/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Customer,Payment,Earning,Game,Payment1,RunningTotal
 # Register your models here.
+admin.site.register(Customer),
+admin.site.register(Payment),
+admin.site.register(Game),
+admin.site.register(Earning),
+admin.site.register(Payment1),
+admin.site.register(RunningTotal)
